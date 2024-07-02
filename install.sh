3bc441aa6f4914989e03f0f87788443c4d6f1c95
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2024-07-02 14:01:42 +0200

    Fix the manpage path & improve (un)installation scripts.
    Fixes the manpage path to always be /usr/local/man/man1/

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
-cp -f version /usr/local/bin
-
-# man page
-gzip -k ./man/version.1
-sudo mkdir -p /usr/local/man/man1/
-sudo cp ./man/version.1.gz /usr/local/share/man/man1/
-rm ./man/version.1.gz
+if [[ $UID != 0 ]]; then
+    echo "Please run this script as root. Aborting."
+    exit 1
+else
+    install -Dm755 ./version /usr/local/bin/version && \
+    # man page
+    gzip -k ./man/version.1 && \
+    install -Dm644 ./man/version.1.gz /usr/local/share/man/man1/version.1.gz && \
+    rm ./man/version.1.gz && \
+    echo "Installation successful." && \
+    exit 0
+fi
+echo "Program Installation failed."
+exit 1
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
-rm -f /usr/local/bin/version
-
-# man page
-sudo rm /usr/local/share/man/man1/version.1.gz
+if [[ $UID != 0 ]]; then
+    echo "Please run this script as root. Aborting."
+    exit 1
+else
+    rm /usr/local/bin/version && \
+    rm /usr/local/share/man/man1/version.1.gz && \
+    echo "Program uninstalled." && \
+    exit 0
+fi
+echo "Program deinstallation failed."
+exit 1
